055bbf54d26d9f1bcec807a916995b0b71bc4044
Assis Júnior <62892419+AssFerj@users.noreply.github.com>
2022-11-10 14:17:57 +0000

    adicionado estilos base de inputs e buttons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,35 @@
-import { Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import React from 'react';
+import { DefaultButton } from '../components/Buttons/DefaultButton';
+import { EmailInput, PasswordInput } from '../components/Inputs/Inputs';
 
 const Login: React.FC = () => {
-  return <Typography variant="h2">Login</Typography>;
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        height: '90vh',
+        margin: '2rem',
+        padding: '3rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItens: 'center'
+      }}
+    >
+      <Typography
+        variant="h3"
+        sx={{
+          textAlign: 'center'
+        }}
+      >
+        Acesse sua conta
+      </Typography>
+      <EmailInput id="login-email-input" label="E-mail" />
+      <PasswordInput id="login-password-input" label="Senha" />
+      <DefaultButton text="Entrar" />
+    </Paper>
+  );
 };
 
 export default Login;

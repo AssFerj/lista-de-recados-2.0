3169dc9ed276017643170612f6da4e60addd43e2
AssFerj <62892419+AssFerj@users.noreply.github.com>
2022-11-11 00:45:01 -0300

    finalizacao login e criacao cadastro

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Cadastro from '../pages/Cadastro';
 import Login from '../pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Login />
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />
   }
 ]);
 

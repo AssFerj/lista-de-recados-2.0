55f5af8f1d5a8a8a904805e5dfbadaac30936b5e
AssFerj <assisjuniorcam@gmail.com>
2022-11-26 19:32:57 -0300

    add cadastro e home pages and config route sys

---
+++ src/pages/Cadastro.tsx
@@ -0,0 +1,8 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Cadastro: React.FC = () => {
+  return <Typography variant="h2">Cadastro</Typography>;
+};
+
+export default Cadastro;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Cadastro from '../pages/Cadastro';
+import Home from '../pages/Home';
 import Login from '../pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Login />
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />
+  },
+  {
+    path: '/home',
+    element: <Home />
   }
 ]);
 

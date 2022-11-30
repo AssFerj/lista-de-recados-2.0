8680c21f0ad62d454c0864e8292d07f6dd0e1854
Assis Júnior <62892419+AssFerj@users.noreply.github.com>
2022-11-30 00:28:00 +0000

    create appbar component at home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,49 +1,54 @@
-import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import ResponsiveAppBar from '../components/ResponsiveAppBar/ResponsiveAppBar';
+
 
 const Home: React.FC = () => {
   return (
-    <Grid
-      columns={12}
-      container
-      direction={'row'}
-      sx={{
-        padding: '3rem',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItens: 'center',
-        height: '100vh'
-      }}
-    >
-      <Grid item xs={12} component="form">
-        <Box
-          sx={{
-            padding: '2rem',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItens: 'center'
-          }}
-        >
-          <Typography
-            variant="h3"
+    <>
+      <ResponsiveAppBar />
+      <Grid
+        columns={12}
+        container
+        direction={'column'}
+        sx={{
+          padding: '3rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItens: 'center',
+          height: 'auto'
+        }}
+      >
+        <Grid item xs={12} component="form">
+          <Box
             sx={{
-              textAlign: 'center'
+              padding: '2rem',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItens: 'center'
             }}
           >
-            Lista de Recados
-          </Typography>
-        </Box>
+            <Typography
+              variant="h3"
+              sx={{
+                textAlign: 'center'
+              }}
+            >
+              Lista de Recados
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            padding: '2rem',
+            width: '50%'
+          }}
+        ></Grid>
       </Grid>
-      <Grid
-        item
-        xs={6}
-        sx={{
-          padding: '2rem',
-          width: '50%'
-        }}
-      ></Grid>
-    </Grid>
+    </>
   );
 };
 

8f26d5f97e3af546589a6043edc8d8cfe460cf57
Assferj <assisjuniorcam@gmail.com>
2023-03-13 11:54:52 -0300

    created pages home, login, register

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,36 @@
-import { AppBar } from '@mui/material';
+import { Container, Grid } from '@mui/material';
+// import bg from "../images/home-bg.png";
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
     <React.Fragment>
-      <AppBar />
+      <Grid
+        container
+        style={{
+          height: '100vh',
+          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          padding: '1.5rem'
+        }}
+      >
+        <Container
+          maxWidth="xl"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-evenly'
+          }}
+        >
+          <Grid item xl={12}>
+            LEFT
+          </Grid>
+          <Grid item xl={12}>
+            RIGHT
+          </Grid>
+        </Container>
+      </Grid>
     </React.Fragment>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,118 @@
-import React from 'react';
-// import { Box, Button, Checkbox, Grid, TextField, Typography } from '@mui/material';
-// import { useNavigate } from 'react-router-dom';
-// import bg from '../images/bg.png';
-// import { useAppDispatch } from '../store/hooks';
-// // import { useAppSelector } from '../store/hooks';
-// import { addLog } from '../store/modules/LoginSlice';
-// // import { selectUserById } from '../store/modules/UsersSlice';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://assisjuniorwm.com.br/">
+        Assis Junior
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const Login: React.FC = () => {
-  return <React.Fragment></React.Fragment>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password')
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              'url(https://images.unsplash.com/photo-1586473219010-2ffc57b0d282?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: t => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Task-In
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="E-mail"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Lembrar de mim" />
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                Acessar
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Esqueceu a senha?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Não tem uma conta? Cadastre-se
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 };
 
 export default Login;

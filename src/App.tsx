d033ea5fd1879b41ee6f7fcf7a730821be23a0f3
Assferj <assisjuniorcam@gmail.com>
2023-04-18 12:03:31 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { themeLight } from './configs/Theme/Theme';
 import AppRoutes from './routes/AppRoutes';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
       <ThemeProvider theme={themeLight}>
         <AppRoutes />
       </ThemeProvider>
+      {/* </PersistGate> */}
     </Provider>
   );
 }

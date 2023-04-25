3d86ec87f5cc42062430580a229ee159d3b790f5
Assferj <assisjuniorcam@gmail.com>
2023-04-25 12:22:38 -0300

    update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import tasksReducer from './modules/tasksSlice';
-// import usersReducer from './modules/usersSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     tasks: tasksReducer,
-//     users: usersReducer
-//   }
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-
 import { configureStore } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';

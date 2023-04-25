908c2fc0679fcbf87f2d4491dcb69c58a7923dfe
AssFerj <assisjuniorcam@gmail.com>
2023-04-24 23:08:13 -0300

    update

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -5,7 +5,7 @@ const Copyright: React.FC = () => {
   return (
     <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
       {'Copyright © '}
-      <Link color="inherit" href="https://curriculum-web-j3bj.vercel.app/conhecimentos.html">
+      <Link color="inherit" href="https://curriculum-web-j3bj.vercel.app/index.html">
         Assis Junior
       </Link>{' '}
       {new Date().getFullYear()}
